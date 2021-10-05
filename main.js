80e79a2d3e9b64c4fbf25796a517d04a0294a693
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-05 14:21:00 +0200

    Az osztály és a konstruktora

---
+++ main.js
@@ -0,0 +1,11 @@
+class Film {
+     nev = "";
+     rendezo = "";
+     kiadasiEv = 0;
+
+     constructor(nev,rendezo,kiadasiEv ) {
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.kiadasiEv = kiadasiEv;
+     }
+}
